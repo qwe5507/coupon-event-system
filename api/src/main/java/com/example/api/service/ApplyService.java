7369090b7e39de4b055bb85967125226eb452ab1
qwe5507 <apvmffkdls@gmail.com>
2023-09-17 19:35:34 +0900

    Redis Set 자료구조 활용하여 한명당 한개의 쿠폰만 생성가능하게 추가

--- api/src/main/java/com/example/api/service/ApplyService.java
+++ api/src/main/java/com/example/api/service/ApplyService.java
@@ -1,6 +1,7 @@
 package com.example.api.service;
 
 import com.example.api.producer.CouponCreateProducer;
+import com.example.api.repository.AppliedUserRepository;
 import com.example.api.repository.CouponCountRepository;
 import com.example.api.repository.CouponRepository;
 import org.springframework.stereotype.Service;
@@ -11,15 +12,24 @@ public class ApplyService {
     private final CouponRepository couponRepository;
     private final CouponCountRepository couponCountRepository;
     private final CouponCreateProducer couponCreateProducer;
+    private final AppliedUserRepository appliedUserRepository;
 
-    public ApplyService(CouponRepository couponRepository, CouponCountRepository couponCountRepository, CouponCreateProducer couponCreateProducer) {
+    public ApplyService(CouponRepository couponRepository, CouponCountRepository couponCountRepository, CouponCreateProducer couponCreateProducer, AppliedUserRepository appliedUserRepository) {
         this.couponRepository = couponRepository;
         this.couponCountRepository = couponCountRepository;
         this.couponCreateProducer = couponCreateProducer;
+        this.appliedUserRepository = appliedUserRepository;
     }
 
     // 쿠폰 발급
     public void apply(Long userId) {
+        Long apply = appliedUserRepository.add(userId);
+
+        if (apply != 1) {
+            // 이미 쿠폰 발급 된 유저
+            return;
+        }
+
         // lock을 활용 하면 성능상 이슈가 발생한다.(lock을 걸어야 하는 범위가 넓어서)
         // redis의 incr명령어를 활용하여 발급된 쿠폰의 갯수를 제어한다.
         Long count = couponCountRepository.increment();
