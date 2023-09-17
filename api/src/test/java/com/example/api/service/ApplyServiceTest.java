3847143883842e7de64bdce58e983a21e65ed012
qwe5507 <apvmffkdls@gmail.com>
2023-09-17 16:48:23 +0900

    test: 여러명 응모 시 동시성 이슈 발생

--- api/src/test/java/com/example/api/service/ApplyServiceTest.java
+++ api/src/test/java/com/example/api/service/ApplyServiceTest.java
@@ -6,6 +6,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -23,7 +28,30 @@ class ApplyServiceTest {
 
         long count = couponRepository.count();
 
-        Assertions.assertThat(count).isEqualTo(1L);
+        assertThat(count).isEqualTo(1L);
+    }
+
+    @Test
+    public void 여러명응모() throws Exception {
+        int threadCount = 1000;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            long userId = i;
+            executorService.submit(() -> {
+                try {
+                    applyService.apply(userId);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        long count = couponRepository.count();
+        assertThat(count).isEqualTo(100);
     }
 
 
