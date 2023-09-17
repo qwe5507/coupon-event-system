3162b0c85f58db29fd3f6396c23888cad461d00e
qwe5507 <apvmffkdls@gmail.com>
2023-09-17 18:09:55 +0900

    kafka producer 설정

--- api/src/main/java/com/example/api/service/ApplyService.java
+++ api/src/main/java/com/example/api/service/ApplyService.java
@@ -1,6 +1,6 @@
 package com.example.api.service;
 
-import com.example.api.domain.Coupon;
+import com.example.api.producer.CouponCreateProducer;
 import com.example.api.repository.CouponCountRepository;
 import com.example.api.repository.CouponRepository;
 import org.springframework.stereotype.Service;
@@ -10,10 +10,12 @@ public class ApplyService {
 
     private final CouponRepository couponRepository;
     private final CouponCountRepository couponCountRepository;
+    private final CouponCreateProducer couponCreateProducer;
 
-    public ApplyService(CouponRepository couponRepository, CouponCountRepository couponCountRepository) {
+    public ApplyService(CouponRepository couponRepository, CouponCountRepository couponCountRepository, CouponCreateProducer couponCreateProducer) {
         this.couponRepository = couponRepository;
         this.couponCountRepository = couponCountRepository;
+        this.couponCreateProducer = couponCreateProducer;
     }
 
     // 쿠폰 발급
@@ -26,6 +28,6 @@ public class ApplyService {
             return;
         }
 
-        couponRepository.save(new Coupon(userId));
+        couponCreateProducer.create(userId);
     }
 }
